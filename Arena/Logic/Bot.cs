a4d5e75b5a309c8392852761f8cd4d842ececf03
hnefal <hnefatl@gmail.com>
2014-05-02 19:25:06 +0100

    Initial

---
+++ Arena/Logic/Bot.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace Arena.Logic
+{
+    public class BotInfo
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Owner { get; set; }
+    }
+
+    public class Bot
+        : BotInfo
+    {
+        protected Process Sub { get; set; }
+
+        public Bot()
+        {
+        }
+
+        public bool Initialise(string Filename)
+        {
+            if (!File.Exists(Filename))
+            {
+                return false;
+            }
+
+            Sub = new Process();
+            Sub.StartInfo.FileName = Filename;
+            Sub.StartInfo.RedirectStandardInput = true;
+            Sub.StartInfo.RedirectStandardOutput = true;
+            Sub.StartInfo.CreateNoWindow = true;
+
+            return true;
+        }
+
+        public void Start()
+        {
+            Sub.Start();
+
+            Name = Read();
+            Version = Read();
+            Owner = Read();
+        }
+
+        public void Write(string Data)
+        {
+            try
+            {
+                Sub.StandardInput.WriteLine(Data);
+            }
+            catch (NullReferenceException)
+            {
+                throw new Exception("Must call Bot.Initialise before reading or writing.");
+            }
+            catch
+            {
+                throw new Exception("Failed to write data.");
+            }
+        }
+        public string Read()
+        {
+            try
+            {
+                return Sub.StandardOutput.ReadLine();
+            }
+            catch (NullReferenceException)
+            {
+                throw new Exception("Must call Bot.Initialise before reading or writing.");
+            }
+            catch
+            {
+                throw new Exception("Failed to read data.");
+            }
+        }
+    }
+}
---
+++ Arena/Logic/Match.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Arena.Logic
+{
+    public abstract class Match
+    {
+        protected List<Bot> Bots { get; set; }
+
+        public Match()
+        {
+            Bots = new List<Bot>();
+        }
+
+        public abstract BotInfo Run();
+
+        public virtual void LoadBot(string Filename)
+        {
+            Bots.Add(new Bot());
+            Bots[Bots.Count - 1].Initialise(Filename);
+        }
+        public void LoadBots(List<string> Filenames)
+        {
+            foreach (string Filename in Filenames)
+            {
+                LoadBot(Filename);
+            }
+        }
+
+        public virtual void StartBots()
+        {
+            for (int x = 0; x < Bots.Count; x++)
+            {
+                Bots[x].Start();
+            }
+        }
+    }
+}
