d66a0572d5c44ad1fcbe9744c19242c7b5edd936
hnefal <hnefatl@gmail.com>
2014-05-16 18:39:23 +0100

    Backup

--- Arena/UI/MainWindow.xaml.cs
+++ Arena/UI/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Shapes;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 using Arena.Logic;
 
@@ -49,235 +50,170 @@ namespace Arena.UI
 
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Easy-access List of all bots in the TreeView
+        /// </summary>
         public List<Bot> AllBots { get; set; }
         public string BotSaveFile { get; set; }
 
         public MainWindow()
         {
             BotSaveFile = "SavedBots";
-            
-            Initialized += new EventHandler((object sender, EventArgs e)=>
-            {
-                Task.Factory.StartNew(new Action(WindowInitialised));
-            });
+
+            Initialized += new EventHandler((object sender, EventArgs e) =>
+                                            {
+                                                Task.Factory.StartNew(new Action(WindowInitialised));
+                                            });
 
             Closing += new System.ComponentModel.CancelEventHandler((object sender, System.ComponentModel.CancelEventArgs e) =>
-            {
-                Task.Factory.StartNew(new Action(WindowClosing));
-            });
+                                                                    {
+                                                                        Task.Factory.StartNew(new Action(WindowClosing));
+                                                                    });
 
             InitializeComponent();
         }
 
         protected void WindowInitialised()
         {
-            SetCovererVisibility(true);
-            ResetProgress(Progress);
+            /*
+            if (Bars.Dispatcher.CheckAccess())
+            {
+                SetCovererVisibility(true);
 
-            List<Task> Tasks = new List<Task>();
-            Tasks.Add(Task.Factory.StartNew(new Action(LoadBots)));
-
-
-            Task.WaitAll(Tasks.ToArray());
-
-            SetCovererVisibility(false);
+                Bars.AddProgressBar("LoadBots");
+                
+                int CompletedTasks=0;
+                BackgroundWorker LoadBotsWorker=new BackgroundWorker();
+                LoadBotsWorker.DoWork+= (Action)(()=>
+                                                 {
+                                                 	LoadBots(Bars.GetProgressBar("LoadBots"));
+                                                 });
+                LoadBotsWorker.ProgressChanged+= (Action)(()=>
+                                                          {
+                                                          	Bars.GetProgressBar("LoadBots").Value++;
+                                                          });
+                LoadBotsWorker.RunWorkerCompleted+=(Action)(()=>
+                                                            {
+                                                            	lock(CompletedTasks)
+                                                            	{
+                                                            		CompletedTasks++;
+                                                            		if(CompletedTasks>=1)
+                                                            		{
+                                                            			SetCovererVisibility(false);
+                                                            		}
+                                                            	}
+                                                            });
+            }
+            else
+            {
+                Bars.Dispatcher.Invoke((Action)WindowInitialised);
+            }
+            */
         }
         protected void WindowClosing()
         {
-            SetCovererVisibility(true);
-            ResetProgress(Progress);
+            Cover.SetVisible(true);
+            Cover.ExecuteOnBar((BarExecutable)((ProgressBar Bar) =>
+            {
+                Bar.Minimum = 0;
+                Bar.Maximum = AllBots.Count;
+                Bar.Value = 0; // Reset progress - we're going to be saving the bot info to disk
+            }));
 
-            List<Task> Tasks = new List<Task>();
-            Tasks.Add(Task.Factory.StartNew(new Action(SaveBots)));
-
-
-            Task.WaitAll(Tasks.ToArray());
-            
-            SetCovererVisibility(false);
+            Cover.SetVisible(false);
         }
 
-        #region Concurrent ProgressBar access methods
-        public delegate void SetCovererVisibilityDelegate(bool Visible);
-        public void SetCovererVisibility(bool Visible)
+        public void LoadBots(ProgressBar Bar)
         {
-            if(Coverer.Dispatcher.CheckAccess())
+            if (!File.Exists(BotSaveFile))
             {
-                if(Progress.Dispatcher.CheckAccess())
+                return;
+            }
+            try
+            {
+                Bar.Maximum = (double)File.ReadAllLines(BotSaveFile).Length; // Increase operation count
+
+                AllBots = new List<Bot>();
+                StreamReader Reader = new StreamReader(BotSaveFile);
+
+                while (true)
                 {
-                    if(Visible)
+                    string Filename = Reader.ReadLine();
+                    if (Filename == null)
+                        break;
+
+                    Bot New = new Bot();
+                    if (New.Initialise(Filename))
                     {
-                        Coverer.Visibility = Visibility.Visible;
-                        Progress.Visibility = Visibility.Visible;
+                        AllBots.Add(New);
+                        Bar.Value++;
+                        Thread.Sleep(1000);
                     }
-                    else
+                }
+                Reader.Close();
+
+                // Loaded all bots
+                #region Postprocessing
+                lock (Bots)
+                {
+                    Bots.Items.Clear();
+                    for (int x = 0; x < AllBots.Count; x++)
                     {
-                        Coverer.Visibility = Visibility.Hidden;
-                        Progress.Visibility = Visibility.Hidden;
-                    }
-                }
-                else
-                {
-                    Progress.Dispatcher.BeginInvoke(new SetCovererVisibilityDelegate(SetCovererVisibility), System.Windows.Threading.DispatcherPriority.Background, Visible);
-                }
-            }
-            else
-            {
-                Coverer.Dispatcher.BeginInvoke(new SetCovererVisibilityDelegate(SetCovererVisibility), System.Windows.Threading.DispatcherPriority.Background, Visible);
-            }
-        }
-
-        public delegate void IncrementProgressDelegate(ProgressBar Bar);
-        public void IncrementProgress(ProgressBar Bar)
-        {
-            if (Bar.Dispatcher.CheckAccess())
-            {
-                lock (Bar)
-                {
-                    Bar.Value++;
-                }
-            }
-            else
-            {
-                Bar.Dispatcher.BeginInvoke(new IncrementProgressDelegate(IncrementProgress), System.Windows.Threading.DispatcherPriority.Background, Bar);
-            }
-        }
-
-        public delegate void IncreaseProgressMaximumDelegate(ProgressBar Bar, double Maximum);
-        public void IncreaseProgressMaximum(ProgressBar Bar, double Maximum)
-        {
-            if (Bar.Dispatcher.CheckAccess())
-            {
-                lock (Bar)
-                {
-                    Bar.Maximum += Maximum;
-                }
-            }
-            else
-            {
-                Bar.Dispatcher.BeginInvoke(new IncreaseProgressMaximumDelegate(IncreaseProgressMaximum), System.Windows.Threading.DispatcherPriority.Background, new object[] { Bar, Maximum });
-            }
-        }
-
-        public delegate void ResetProgressDelegate(ProgressBar Bar);
-        public void ResetProgress(ProgressBar Bar)
-        {
-            if(Bar.Dispatcher.CheckAccess())
-            {
-                lock(Bar)
-                {
-                    Bar.Value = 0;
-                    Bar.Minimum = 0;
-                    Bar.Maximum = 0;
-                }
-            }
-            else
-            {
-                Bar.Dispatcher.BeginInvoke(new ResetProgressDelegate(ResetProgress), System.Windows.Threading.DispatcherPriority.Background, Bar);
-            }
-        }
-        #endregion
-
-        public delegate void LoadBotsDelegate();
-        public void LoadBots()
-        {
-            if (Bots.Dispatcher.CheckAccess())
-            {
-                if (!File.Exists(BotSaveFile))
-                {
-                    return;
-                }
-                try
-                {
-                    IncreaseProgressMaximum(Progress, (int)File.ReadAllLines(BotSaveFile).Length); // Increase operation count
-
-                    AllBots = new List<Bot>();
-                    StreamReader Reader = new StreamReader(BotSaveFile);
-
-                    while (true)
-                    {
-                        string Filename = Reader.ReadLine();
-                        if (Filename == null)
-                            break;
-
-                        Bot New = new Bot();
-                        if (New.Initialise(Filename))
+                        #region Find owner
+                        TreeViewItem Owner = null;
+                        for (int y = 0; y < Bots.Items.Count; y++) // Search for owner in tree
                         {
-                            AllBots.Add(New);
-                            IncrementProgress(Progress); // Increase progress
-                            Thread.Sleep(5000);
+                            if ((string)((TreeViewItem)Bots.Items[y]).Header == AllBots[x].Owner) // Same owner
+                            {
+                                Owner = (TreeViewItem)Bots.Items[y];
+                                break;
+                            }
                         }
-                    }
-                    Reader.Close();
-
-                    // Loaded all bots
-                    #region Postprocessing
-                    lock (Bots)
-                    {
-                        Bots.Items.Clear();
-                        for (int x = 0; x < AllBots.Count; x++)
+                        if (Owner == null) // Create new owner
                         {
-                            #region Find owner
-                            TreeViewItem Owner = null;
-                            for (int y = 0; y < Bots.Items.Count; y++) // Search for owner in tree
-                            {
-                                if ((string)((TreeViewItem)Bots.Items[y]).Header == AllBots[x].Owner) // Same owner
-                                {
-                                    Owner = (TreeViewItem)Bots.Items[y];
-                                    break;
-                                }
-                            }
-                            if (Owner == null) // Create new owner
-                            {
-                                Owner = new TreeViewItem();
-                                Owner.Header = AllBots[x].Owner;
-                                Bots.Items.Add(Owner);
-                            }
-                            #endregion
-                            #region Find name
-                            TreeViewItem Name = null;
-                            foreach (TreeViewItem n in Owner.Items)
-                            {
-                                if ((string)n.Header == AllBots[x].Name)
-                                {
-                                    Name = n;
-                                }
-                            }
-                            if (Name == null)
-                            {
-                                Name = new TreeViewItem();
-                                Name.Header = AllBots[x].Name;
-                                Owner.Items.Add(Name);
-                            }
-                            #endregion
-                            BotTreeViewItem NewBot = new BotTreeViewItem(AllBots[x]);
-                            NewBot.MouseDoubleClick += BotSelected;
-                            Name.Items.Add(NewBot);
-                        }
-                        #region Add ToolTips
-                        for (int x = 0; x < Bots.Items.Count; x++)
-                        {
-                            (Bots.Items[x] as TreeViewItem).ToolTip = "Bots: " + (Bots.Items[x] as TreeViewItem).Items.Count;
-                            for (int y = 0; y < ((TreeViewItem)Bots.Items[x]).Items.Count; y++)
-                            {
-                                ((TreeViewItem)(((TreeViewItem)Bots.Items[x]).Items[y])).ToolTip = "Versions: " + ((TreeViewItem)(((TreeViewItem)Bots.Items[x]).Items[y])).Items.Count;
-                            }
+                            Owner = new TreeViewItem();
+                            Owner.Header = AllBots[x].Owner;
+                            Bots.Items.Add(Owner);
                         }
                         #endregion
+                        #region Find name
+                        TreeViewItem Name = null;
+                        foreach (TreeViewItem n in Owner.Items)
+                        {
+                            if ((string)n.Header == AllBots[x].Name)
+                            {
+                                Name = n;
+                            }
+                        }
+                        if (Name == null)
+                        {
+                            Name = new TreeViewItem();
+                            Name.Header = AllBots[x].Name;
+                            Owner.Items.Add(Name);
+                        }
+                        #endregion
+                        BotTreeViewItem NewBot = new BotTreeViewItem(AllBots[x]);
+                        NewBot.MouseDoubleClick += BotSelected;
+                        Name.Items.Add(NewBot);
+                    }
+                    #region Add ToolTips
+                    for (int x = 0; x < Bots.Items.Count; x++)
+                    {
+                        (Bots.Items[x] as TreeViewItem).ToolTip = "Bots: " + (Bots.Items[x] as TreeViewItem).Items.Count;
+                        for (int y = 0; y < ((TreeViewItem)Bots.Items[x]).Items.Count; y++)
+                        {
+                            ((TreeViewItem)(((TreeViewItem)Bots.Items[x]).Items[y])).ToolTip = "Versions: " + ((TreeViewItem)(((TreeViewItem)Bots.Items[x]).Items[y])).Items.Count;
+                        }
                     }
                     #endregion
                 }
-                catch
-                {
-                    throw new Exception("Failed to load all bots.");
-                }
+                #endregion
             }
-            else
+            catch
             {
-                Bots.Dispatcher.BeginInvoke(new LoadBotsDelegate(LoadBots), System.Windows.Threading.DispatcherPriority.Background);
+                throw new Exception("Failed to load all bots.");
             }
         }
-
-
 
         void BotSelected(object sender, MouseButtonEventArgs e)
         {
